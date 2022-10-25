f244e0488ee122ff940c09274920f568a437e4b3
Nuha <azazi.nuha@gmail.com>
2022-10-26 00:16:13 +0200

    ex4 3,4

--- Week2/homework/ex4.js
+++ Week2/homework/ex4.js
@@ -29,12 +29,11 @@ function selectQuery(sql){
 selectQuery("select count(*) from (SELECT research_Papers.paper_id from research_Papers left join authors ON authors.author_id = research_Papers.author_id where authors.gender= 'female') as female_authors")
 
 // Average of the h-index of all authors per university.
-selectQuery("SELECT authors.author_name,authors.university,AVG(authors.h_index )FROM meetup.authors GROUP BY university");
+selectQuery("SELECT authors.university, AVG(authors.h_index) FROM authors GROUP by authors.university");
 // Sum of the research papers of the authors per university.
-
+selectQuery("SELECT authors.university, COUNT(research_Papers.author_id) AS count FROM meetup.authors left join meetup.research_Papers on authors.author_id = research_Papers.author_id GROUP by authors.university");
 // Minimum and maximum of the h-index of all authors per university.
-
-SELECT t2.author_name as author, t1.author_name as mentor FROM meet.upauthors as t1 INNER JOIN meetup.authors as t2 ON t1.author_id = t2.mentor
+selectQuery("SELECT authors.university, MIN(authors.h_index) ,MAX(authors.h_index) FROM authors GROUP by authors.university");
 
 
 
